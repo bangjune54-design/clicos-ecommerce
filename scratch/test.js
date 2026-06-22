27c5c077736afaf8a78f4b013c8a22bb254483c8
Junsung Bang <junsungbang@MacBook-Pro-2.local>
2026-06-22 16:21:22 +0900

    chore: remove temporary scratch file

--- scratch/test.js
+++
@@ -1,6 +0,0 @@
-// Simple test runner for our utilities
-import { INITIAL_INVENTORY, INITIAL_BRANDS } from '../src/utils/syncedDefaults.js';
-
-console.log("Synced defaults loaded successfully.");
-console.log("Products count:", INITIAL_INVENTORY.length);
-console.log("Brands count:", INITIAL_BRANDS.length);
